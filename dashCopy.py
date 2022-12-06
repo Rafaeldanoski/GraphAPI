571325e58977004f199d5fe390621df69bd8e10b
rafae <rdanoski@gmail..com>
2022-12-06 13:07:07 -0300

    commit adsetCheckbox

--- dashCopy.py
+++ dashCopy.py
@@ -20,7 +20,12 @@ temp = st.multiselect("TEMPERATURA",(list(df['TEMPERATURA'].unique())),'FRIO')
 product = st.multiselect("PRODUTO",(list(df['product'].unique())),default=['DIP'])
 kind = st.multiselect("TIPO",(list(df['kind'].unique())),default=['ESCALA'])
 objective = st.multiselect("OBJETIVO",(list(df['objective'].unique())),default=['CONVERSIONS'])
-#adset = st.multiselect("ADSET",(list(df['adset_name'].unique())))
+
+adset = st.checkbox('ADSET')
+if adset:
+    adset = st.multiselect("ADSET",(list(df['adset_name'].unique())))
+else:
+    adset = list(df['adset_name'].unique())
 
 start_date, end_date = st.date_input('start date  - end date :', [datetime.today()-timedelta(30), datetime.today()])
 if start_date <= end_date:
@@ -34,11 +39,11 @@ df_filter = df[(df['product'].isin(product))
             & (pd.to_datetime(df['date_start'])>=pd.to_datetime(start_date))
             & (pd.to_datetime(df['date_start'])<=pd.to_datetime(end_date))
             & df['TEMPERATURA'].isin(temp)
-            #& df['adset_name'].isin(adset)
+            & df['adset_name'].isin(adset)
             ]
             
 
-agg_full = df_filter.groupby(['product','kind']).agg({'clicks':'sum', 'impressions':'sum', 'reach':'sum','spend':'sum', 'purchase_value':'sum', 
+agg_full = df_filter.groupby(['product','kind','adset_name']).agg({'clicks':'sum', 'impressions':'sum', 'reach':'sum','spend':'sum', 'purchase_value':'sum', 
                                      'purchase':'sum','ctr_acc':'first','video_25':'sum','video_50':'sum','video_75':'sum'})
 
 agg_full['frequency'] = agg_full['impressions'] / agg_full['reach']
@@ -50,8 +55,10 @@ agg_full['cpa'] = agg_full['spend'] / agg_full['purchase']
 #agg_full['score'] = agg_full['ctr'] * agg_full['clicks']
 #agg_full['ctr_delta'] = ((agg_full['ctr'] / agg_full['ctr_acc']) - 1) * 100
 
+agg_full.replace([np.inf, -np.inf], 0, inplace=True)
+
 agg_full = agg_full.drop(['purchase_value', 'ctr_acc','reach'], axis=1)
-agg_full
+st.dataframe(agg_full, use_container_width=True)
 
 ############ Analítico ####################
 classify = st.selectbox(
@@ -175,10 +182,12 @@ scatter_full = alt.Chart(agg).mark_point().encode(
 
 st.altair_chart(scatter_full, use_container_width=False)
 
+
 ###################### KPI por anúncio #######################
 st.write("""
  # KPI's por anúncio
 """)
+
 option = st.selectbox(
     "Anúncio",
     (agg.index))
@@ -187,6 +196,7 @@ df_graph = df[(df['name']==option)
             & (df['product'].isin(product))
             & (df['kind'].isin(kind))
             & df['TEMPERATURA'].isin(temp)
+            & df['adset_name'].isin(adset)
             ]
 
 st.write("""
