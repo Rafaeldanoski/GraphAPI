63768778d163147dbe0f8680e015599b53494a9b
rafae <rdanoski@gmail..com>
2022-12-15 13:54:05 -0300

    commitOnePage20221215

--- pages/1_Report_One_Page.py
+++ pages/1_Report_One_Page.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
+import altair as alt
 
 ############## DATASET #######################
 df = pd.read_csv('ads_full.csv', sep=';')
@@ -30,16 +31,19 @@ with col_prod:
 
 st.markdown("<h1 style='text-align: center;'>ETAPA 1</h1>", unsafe_allow_html=True)
 
+df_filter = df[(df['product'].isin([product]))
+              & (pd.to_datetime(df['date_start'])>=pd.to_datetime(start_date))
+              & (pd.to_datetime(df['date_start'])<=pd.to_datetime(end_date))
+              & (df['impressions'] > 0)
+               ]            
+df_filter.reset_index(drop=True, inplace=True)
+
 st.write("""
 ### Conjunto de Anúncio: 02 - [Etapa 1: Teste de criativo] - Semelhantes a compradores 3%
 """)
 
 adset_test = ['02 - [Etapa 1: Teste de criativo] - Semelhantes a compradores 3%']
-df_ad_test = df[df['adset_name'].isin(adset_test)
-              & df['product'].isin([product])
-              & (pd.to_datetime(df['date_start'])>=pd.to_datetime(start_date))
-              & (pd.to_datetime(df['date_start'])<=pd.to_datetime(end_date))
-              & (df['impressions'] > 0)
+df_ad_test = df_filter[df_filter['adset_name'].isin(adset_test)
                ]
 
 df_ad_test.reset_index(drop=True, inplace=True)
@@ -81,25 +85,94 @@ for i in range(len(agg)):
 
 st.markdown("<h1 style='text-align: center;'>ETAPA 2</h1>", unsafe_allow_html=True)
 
-ad = '45_Dashboardnyc'
+#ad = '45_Dashboardnyc'
 
-df_pub_test = df[(df['product'].isin([product]))
-              & (pd.to_datetime(df['date_start'])>=pd.to_datetime(start_date))
-              & (pd.to_datetime(df['date_start'])<=pd.to_datetime(end_date))
-              & (df['impressions'] > 0)
-              & (df['name'] == ad)
+import re
+df_filter['etapa'] = ""
+for i in range(len(df_filter['adset_name'])):
+    a_string = df_filter['adset_name'][i]
+    result = re.findall(r"\[([][A-Za-z0-9_: ]+)\]", a_string)
+    try:
+        df_filter['etapa'][i] = result[0]
+    except:
+        df_filter['etapa'][i] = result
+
+df_pub_test = df_filter[(df_filter['etapa'] == 'Etapa 2: Teste de publico')
                ]            
 df_pub_test.reset_index(drop=True, inplace=True)
 
-import re
-df_pub_test['etapa'] = ""
-for i in range(len(df_pub_test['adset_name'])):
-    a_string = df_pub_test['adset_name'][i]
+agg_pub_test = df_pub_test.groupby(['name','adset_name']).agg({'name':'last','adset_name':'last','insta_link':'last','clicks':'sum', 'impressions':'sum', 'spend':'sum'})
+
+agg_pub_test['ctr'] = agg_pub_test['clicks'] / agg_pub_test['impressions'] * 100
+
+agg_pub_test.sort_values(by='ctr', ascending=False, inplace=True)
+
+for a in range(len(agg_pub_test['name'].unique())):
+    agg_pub_test_write = agg_pub_test[agg_pub_test['name']==(agg_pub_test['name'].unique()[a])
+                                     ]
+
+    st.write(f"""
+    ### Anúncio: {agg_pub_test['name'].unique()[a]}
+    """)
+
+    best_adset = ['', 0]
+    for i in range(len(agg_pub_test_write)):
+        if (agg_pub_test_write['ctr'][i] > best_adset[1]) and (agg_pub_test_write['impressions'][i] > 10000):
+            best_adset[0] = agg_pub_test_write.index[i]
+            best_adset[1] = agg_pub_test_write['ctr'][i]
+
+    for i in range(len(agg_pub_test_write)):
+        name = agg_pub_test_write['adset_name'][i]
+        if agg_pub_test_write['impressions'][i] < 10000:
+            st.write(f"#### - ➖ {name} --------> Dados insuficientes para análise - Impressões: "+ str(agg_pub_test_write['impressions'][i]) + "/ Gasto: R$ " + str(round(agg_pub_test_write['spend'][i],2)))
+        elif (agg_pub_test_write.index[i] == best[0]) and (agg_pub_test_write.ctr[i] >= 1.5):
+            st.write(f"#### - ✅ {name} --------> Promover à Etapa 3 - CTR: " + str(round(agg_pub_test_write['ctr'][i],2)))
+        else:
+            st.write(f"#### - 🚫 {name} --------> Pausar veiculação - CTR: " + str(round(agg_pub_test_write['ctr'][i],2)))
+
+
+
+############ ETAPA 3 ######################
+
+st.markdown("<h1 style='text-align: center;'>ETAPA 3</h1>", unsafe_allow_html=True)
+
+step_prod = ['Etapa 1: Teste de criativo','Etapa 2: Teste de publico']
+
+df_ad_prod = df[(df['product'].isin([product]))
+              & (df['impressions'] > 0)
+              & (pd.to_datetime(df['date_start'])>='2022-12-01')
+               ]
+df_ad_prod.reset_index(drop=True, inplace=True)
+
+df_ad_prod['etapa'] = ""
+for i in range(len(df_ad_prod['adset_name'])):
+    a_string = df_ad_prod['adset_name'][i]
     result = re.findall(r"\[([][A-Za-z0-9_: ]+)\]", a_string)
     try:
-        df_pub_test['etapa'][i] = result[0]
+        df_ad_prod['etapa'][i] = result[0]
     except:
-        df_pub_test['etapa'][i] = result
+        df_ad_prod['etapa'][i] = result
 
+df_ad_prod = df_filter[df_filter['etapa'].isin(step_prod)]
 
-df_pub_test
+df_ad_prod.reset_index(drop=True, inplace=True)
+
+df_ad_prod_graph = df_ad_prod[df_ad_prod['etapa']=='Etapa 2: Teste de publico']
+df_ad_prod_graph.reset_index(drop=True, inplace=True)
+
+for g in range(len(df_ad_prod_graph['name'].unique())):
+
+    st.write(f"""
+    ### Anúncio: {df_ad_prod_graph['name'].unique()[g]}
+    """)
+
+    df_line = df_ad_prod[df_ad_prod['name']==df_ad_prod_graph['name'].unique()[g]]
+
+    ctr_line = alt.Chart(df_line).mark_line(point=alt.OverlayMarkDef(color="blue")).encode(
+    x='date_start',
+    y='ctr_acc',
+    color='adset_name:N',
+    strokeDash='month'
+    )
+
+    st.altair_chart(ctr_line.interactive(), use_container_width=True)
