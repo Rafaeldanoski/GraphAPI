01be2e882622694f052a135365d473f08e539628
rafae <rdanoski@gmail..com>
2022-12-22 09:24:23 -0300

    update

--- pages/2_Dados_Gerais.py
+++ pages/2_Dados_Gerais.py
@@ -31,16 +31,31 @@ df_filter = df[(df['product'].isin(product))
               & (pd.to_datetime(df['date_start'])>=pd.to_datetime(start_date))
               & (pd.to_datetime(df['date_start'])<=pd.to_datetime(end_date))
               ]
-df_filter
+
+df['yearmonth'] = (df['year'].map(str) + df['month'].map(str)).map(int)
+
+df_graph = df[(df['product'].isin(product))
+              ]
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric(label="Gasto", value=df_filter['spend'].sum().round(2))
+col2.metric(label="Nº Vendas", value=df_filter['purchase'].sum().round(2))
+col3.metric(label="R$ Vendas", value=df_filter['purchase_value'].sum().round(2))
+col4.metric(label="ROAS", value=(df_filter['purchase_value'].sum()/df_filter['spend'].sum()).round(2))
 
 st.write("""
  Spend X Tempo
 """)
 
-spend_line = alt.Chart(df).mark_bar().encode(
-    x='month',
+spend_bar = alt.Chart(df_graph).mark_bar().encode(
+    x=alt.X(field='yearmonth'),
     y=alt.Y(field='spend', aggregate='sum'),
 
 ).interactive()
 
-st.altair_chart(spend_line)
+purchase_line = alt.Chart(df_graph).mark_line(color='red').encode(
+    x=alt.X(field='yearmonth'),
+    y=alt.Y(field='purchase_value', aggregate='sum'),
+).interactive()
+
+st.altair_chart(spend_bar + purchase_line, use_container_width=True)
